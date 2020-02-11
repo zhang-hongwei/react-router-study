efe66b5e4abbd3b5ab421f39d8966b1a8a6652a1
zhanghongwei <zhang123910@live.com>
2020-02-11 21:20:48 +0800

    feat: 添加replace

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
-import logo from "./logo.svg";
+import React, { useState } from 'react';
+import logo from './logo.svg';
 // import App from './App';
-import A from "./pages/a";
-import B from "./pages/b";
-import "./App.css";
-import { Router, Route, Switch, Link } from "react-router-dom";
-import MyContext from "./pages/context/mycontext";
-import RouterWrap from "./router";
+import A from './pages/a';
+import B from './pages/b';
+import './App.css';
+import { Router, Route, Switch, Link } from 'react-router-dom';
+import MyContext from './pages/context/mycontext';
+import RouterWrap from './router';
 
 function App() {
     const handleClick = () => {
@@ -14,16 +14,27 @@ function App() {
 
         // window.history.go(-1)
         window.history.pushState(
-            { foo: "bar", key: "123123123" },
-            "page1",
-            "http://localhost:3000/b"
+            { foo: 'bar', key: '123123123' },
+            'page1',
+            'http://localhost:3000/b'
+        );
+    };
+
+    const handleClick1 = () => {
+        window.history.replaceState(
+            { foo: 'bar', key: '123123123' },
+            'page1',
+            'http://localhost:3000/c'
         );
     };
 
     let [val, setval] = useState(0);
     return (
         <MyContext.Provider value={{ val: val }}>
-            <button onClick={handleClick}> click</button>
+            <button onClick={handleClick}>pushState</button>
+            <br />
+            <button onClick={handleClick1}>replaceState</button>
+            <br />
             <RouterWrap></RouterWrap>
         </MyContext.Provider>
     );
